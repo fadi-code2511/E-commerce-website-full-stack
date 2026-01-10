4966c932fdab314ed95052504258f9fac8e78c64
Fadi nasr <nasr.fadi2511@gmail.com>
2026-01-10 13:47:33 +0200

    creating the new arrival section

--- frontend/src/components/Products/GenderCollectionSection.jsx
+++ frontend/src/components/Products/GenderCollectionSection.jsx
@@ -5,22 +5,24 @@ import { Link } from "react-router-dom";
 
 const GenderCollectionSection = () => {
   return (
-    <div className='flex  md:flex-row flex-col'>
-        <div className='relative m-5 bg-gray-100 p-5'>
-            <img src={manCollectionImg} alt="" className='w-full'/>
+    <section>
+    <div className='flex container mx-auto  md:flex-row flex-col gap-8'>
+        <div className='relative m-5 bg-gray-100 p-5 flex-1'>
+            <img src={manCollectionImg} alt="" className='w-full object-cover h-[700px]'/>
             <div className='  p-3 text-center absolute bottom-11 left-9 bg-white/70 py-2 '>
                 <h3 className='text-xs md:text-lg font-bold'>Men's collection</h3>
                 <Link to="#" href="#" className='tex-black  text-xs md:text-sm  underline '>shop</Link>
             </div>
         </div>
-        <div className='relative m-5 bg-gray-100 p-5'>
-            <img src={womanCollectionImg} alt="" className='w-full object-cover h-full'/>
+        <div className='relative m-5 bg-gray-100 p-5 flex-1'>
+            <img src={womanCollectionImg} alt="" className='w-full object-cover h-[700px]'/>
             <div className=' p-3 text-center absolute bottom-11 left-9 bg-white/70  '>
                 <h3 className='text-xs md:text-lg font-bold'>Women's collection</h3>
                 <Link to="#" href="#" className='tex-black  text-xs md:text-sm underline '>shop</Link>
             </div>
         </div>
     </div>
+    </section>
   )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from "../components/Layout/Hero"
 import GenderCollectionSection from "../components/Products/GenderCollectionSection"
+import NewArrivals from "../components/Products/NewArrivals"
 
 function Home(){
     return <div>
         <Hero />
         <GenderCollectionSection />
+        <NewArrivals />
     </div>
 }
 
