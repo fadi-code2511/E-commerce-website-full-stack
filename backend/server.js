614efce4eb0261a3ba8073ea4955f1c3dc1016b0
Fadi nasr <nasr.fadi2511@gmail.com>
2026-01-28 14:46:45 +0200

    chore:setup-backend and db

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv" 
+import connectDB from "./config/db.js"
+
+const app=express()
+
+//middleware
+app.use(express.json())  // we use epress.json() to parsing json payload and make the data accessable throug res.body (convert from json raw to=> js object)
+app.use(cors()); //allows the frontend to access backend APIs when they are on different origins (exp: frontend:localhost:3000 ,backend:localhost:9000)
+
+dotenv.config() // to load the envirement variables we call config()
+
+//conncet to mongodb
+connectDB()
+
+const PORT=process.env.PORT || 3000 ;
+
+
+app.get("/",(req,res)=>{
+    res.send("Wlc there")
+})
+
+app.listen(PORT,()=>{
+    console.log("server is running on prot:",PORT)
+})
