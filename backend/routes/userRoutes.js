504d00fa92a52f50253e8d073174903e0ea0a9d2
Fadi nasr <nasr.fadi2511@gmail.com>
2026-01-29 12:05:26 +0200

    feat: add user login with jwt BE

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -40,5 +40,40 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentails" });
 
+    const isMatched = await user.matchPassword(password);
+    if (!isMatched)
+      return res.status(400).json({ message: "Invalid Credentails" });
+
+    //creat jwt payload
+    const payload = { user: { id: user._id, role: user.role } };
+    // return token
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "40h" },
+      (err, token) => {
+        if (err) return res.status(500).json("Token generation faild");
+
+        res.json({
+          user: {
+            name: user.name,
+            email: user.email,
+            role:user.role,
+          },
+          token
+        });
+      },
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server erorr");
+  }
+});
 export default router;
