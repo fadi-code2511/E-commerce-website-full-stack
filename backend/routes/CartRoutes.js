d2e0ab5e78e9868f93e7daed54bff005ac5a7508
Fadi nasr <nasr.fadi2511@gmail.com>
2026-02-01 13:34:18 +0200

    feat: add update quantity route , to update the product quantity in the cart BE

--- backend/routes/CartRoutes.js
+++ backend/routes/CartRoutes.js
@@ -1,78 +1,126 @@
 import express from "express";
 import Cart from "../models/Cart.js";
 import Product from "../models/Product.js";
+import protect from "../middleware/authMiddleware.js";
 
-const router=express.Router();
+const router = express.Router();
 
 // POST api/cart
 // add a product to cart for guest and logged in user
 // access public
-const getcart=async(userId,guestId)=>{
-    if (userId){
-        return await Cart.findOne({userId});
-    }
-    if (guestId){
-        return await Cart.findOne({guestId});
-    }
-    return null;
-}
+const getcart = async (userId, guestId) => {
+  if (userId) {
+    return await Cart.findOne({ userId });
+  }
+  if (guestId) {
+    return await Cart.findOne({ guestId });
+  }
+  return null;
+};
 
-router.post("/",async (req,res) => {
-    try {
-        
-    
-    const {productId,size,color,userId,guestId,quantity}=req.body;
-    const product= await Product.findById(productId)
-    if (!product) res.status(404).json({message:"product not found"})
-    
+router.post("/", async (req, res) => {
+  try {
+    const { productId, size, color, userId, guestId, quantity } = req.body;
+
+    const product = await Product.findById(productId);
+    if (!product) res.status(404).json({ message: "product not found" });
+
     // determine if the user is logged in or guest
-    const cart=await getcart(userId,guestId);
+    const cart = await getcart(userId, guestId);
     // if cart is exist , update the cart
-    if(cart){
-        const productIndex=cart.products.findIndex((product)=>
-            product.productId.toLocaleString()=== productId && product.size===size && product.color===color
-        )
-        if(productIndex>-1){
-            //if the product is exist, update the quantity
-            cart.products[productIndex].quantity+=parseInt(quantity);
-        }else{
-            //product not exist, add new product to the cart
-            cart.products.push({
-                productId,
-                size,
-                color,
-                quantity,
-                image:product.images[0].url,
-                price:product.price,
-                name:product.name,
-            })
-        }
-        cart.totalPrice=cart.products.reduce((accumulator,item)=>accumulator+item.price*item.quantity,0)
-        await cart.save();
-        res.json(cart);
-    }else{
-         const newCart=await  Cart.create({
-            user:userId?userId:undefined,
-            guestId:guestId?guestId:"guest_" + new Date().getTime(),
-            products:[
-                {
-                    productId,
-                    size,
-                    color,
-                    quantity,
-                    name:product.name,
-                    price:product.price,
-                    image:product.images[0].url,
-                }
-            ],
-            totalPrice:product.price*quantity,
-         })
-        res.status(201).json(newCart)
-    }} catch (error) {
-        console.error(error);
-        res.status(500).json({message:"Server Error"})
+    if (cart) {
+      const productIndex = cart.products.findIndex(
+        (product) =>
+          product.productId.toLocaleString() === productId &&
+          product.size === size &&
+          product.color === color,
+      );
+      if (productIndex > -1) {
+        //if the product is exist, update the quantity
+        cart.products[productIndex].quantity += parseInt(quantity);
+      } else {
+        //product not exist, add new product to the cart
+        cart.products.push({
+          productId,
+          size,
+          color,
+          quantity,
+          image: product.images[0].url,
+          price: product.price,
+          name: product.name,
+        });
+      }
+      cart.totalPrice = cart.products.reduce(
+        (accumulator, item) => accumulator + item.price * item.quantity,
+        0,
+      );
+      await cart.save();
+      res.json(cart);
+    } else {
+      const newCart = await Cart.create({
+        user: userId ? userId : undefined,
+        guestId: guestId ? guestId : "guest_" + new Date().getTime(), // to avoid collisions(crash), we can use uuid package(library)
+        products: [
+          {
+            productId,
+            size,
+            color,
+            quantity,
+            name: product.name,
+            price: product.price,
+            image: product.images[0].url,
+          },
+        ],
+        totalPrice: product.price * quantity,
+      });
+      res.status(201).json(newCart);
     }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
 
-})
+// route: api/cart
+//desc: update the product quntity in the cart of guest or logged in user
+// access public
+router.put("/", async (req, res) => {
+  try {
+    const { productId, size, color, userId, guestId, quantity } = req.body;
 
-export default router
+    const product = await Product.findById(productId);
+    if (!product) res.status(404).json({ message: "product not found" });
+
+    // determine if the user is logged in or guest
+    const cart = await getcart(userId, guestId);
+    // if cart is exist , update the cart
+    if (!cart)  return res.status(404).json({message:"Cart not found"});
+      const productIndex = cart.products.findIndex(
+        (product) =>
+          product.productId.toLocaleString() === productId &&
+          product.size === size &&
+          product.color === color,
+      );
+      if (productIndex > -1) {
+        //if the product is exist, update the quantity
+        if(quantity>0){
+            cart.products[productIndex].quantity = parseInt(quantity);
+        }else{
+            cart.products.splice(productIndex,1); // qauntity=0, remove the product 
+        }
+      cart.totalPrice = cart.products.reduce(
+        (accumulator, item) => accumulator + item.price * item.quantity,
+        0,
+      );
+      await cart.save();
+      return res.json(cart);
+    }else{
+       return res.status(404).json({message:"product not found"})
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+export default router;
