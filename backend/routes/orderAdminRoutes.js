2edafcc6cc07319d1508d48cbd323ba3a8f91529
Fadi nasr <nasr.fadi2511@gmail.com>
2026-02-10 18:55:45 +0200

    feat: add delete order route, to delete an order BE

--- backend/routes/orderAdminRoutes.js
+++ backend/routes/orderAdminRoutes.js
@@ -46,5 +46,21 @@ router.put("/:id",protect,admin,async (req,res) => {
     }
 })
 
-
+//Route: DELETE api/admin/orders/:id
+//DESC: delete an order
+//access: private(admin)
+router.delete("/:id",protect,admin,async (req,res) => {
+    try {
+        const order=await Order.findById({_id:req.params.id});
+        if(order){
+          await order.deleteOne()
+            res.json({message:"order deleted"});
+        }else{
+            res.status(404).json({Message:"no order found"})
+        }
+    } catch (error) {
+            console.error(error);
+    res.status(500).json({ message: "Server Error" });
+    }
+})
 export default router;
