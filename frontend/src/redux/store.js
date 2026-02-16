05f88f93322ccd6e4109ef5f0d1f6752373722e6
Fadi nasr <nasr.fadi2511@gmail.com>
2026-02-16 12:57:18 +0200

    feat: add checkout slice, to create checkout session

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import {authReducer} from "../redux/slices/authSlice.js"
 import { productReducer } from './slices/productsSlice.js'
 import { cartReducer } from './slices/cartSlice.js'
+import { checkoutReducer } from './slices/checkoutSlice.js'
 
 const store=configureStore({
   reducer: {
     auth:authReducer,
     products:productReducer,
-    cart:cartReducer
+    cart:cartReducer,
+    checkout:checkoutReducer,
   }
 })
 
