545c700ac3cacc762f9e764b1ccf3c8cfd36a4d9
Fadi nasr <nasr.fadi2511@gmail.com>
2026-02-16 12:07:18 +0200

    feat: add cart slices, to fetch,add,update,remove,merge

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import {authReducer} from "../redux/slices/authSlice.js"
 import { productReducer } from './slices/productsSlice.js'
+import { cartReducer } from './slices/cartSlice.js'
 
 const store=configureStore({
   reducer: {
     auth:authReducer,
     products:productReducer,
+    cart:cartReducer
   }
 })
 
